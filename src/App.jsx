0db3203753ba005e9a37228d49b458bfc6c129c2
AhaD <abuahad35188@gmail.com>
2025-04-28 11:36:32 +0600

    react project set up for fremation animation

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+ 
+import { motion } from "motion/react"
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <h2>hello bangladesh</h2>
+     <motion.div animate={{ rotate: 360 }} className="box"></motion.div>
+    </>
+  )
+}
+
+export default App
